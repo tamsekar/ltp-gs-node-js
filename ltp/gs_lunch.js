ccec4fd417eaf73627f0cbb325ceda3c3c30a23e
atamsekar <atamsekar@pulsesecure.net>
2022-05-05 17:56:29 -0700

    gs-section first program

---
+++ ltp/gs_lunch.js
@@ -0,0 +1,8 @@
+/* Variables */
+lunch = '12 inch pizza'
+sizeofParty = 4
+totalCost = 14.99
+averageCost = totalCost / sizeofParty
+message = 'Your lunch of ' + lunch + ' for ' + sizeofParty + ' people will cost you ' + averageCost
+
+console.log(message)
